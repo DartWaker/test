3d3b1fc280f9c68feea00eeebe8479d980858480
DartWaker <democracy.potato@gmail.com>
2024-11-20 12:55:03 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// Инициализация ScrollSmoother для плавного прокручивания
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.5, // Плавность прокрутки
-  effects: true // Включены эффекты
-});
+// Установка плавного прокручивания только для ПК
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5, // Плавность прокрутки
+    effects: true // Включены эффекты
+  });
+}
 
 // Анимация для секции hero
 gsap.fromTo('.hero-section', { opacity: 1 }, {
@@ -19,7 +21,7 @@ gsap.fromTo('.hero-section', { opacity: 1 }, {
   }
 });
 
-// Анимация для левых элементов галереи
+// Анимация для левых элементов галереи с проверкой на мобильные устройства
 let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
 itemsL.forEach(item => {
   gsap.fromTo(item, { opacity: 0, x: -50 }, {
@@ -28,12 +30,14 @@ itemsL.forEach(item => {
       trigger: item,
       start: '-850',
       end: '-100',
-      scrub: true
+      scrub: true,
+      // Ограничение анимации на мобильных устройствах
+      onEnter: () => { if (ScrollTrigger.isTouch === 1) gsap.to(item, { opacity: 1, x: 0 }); }
     }
   });
 });
 
-// Анимация для правых элементов галереи
+// Анимация для правых элементов галереи с проверкой на мобильные устройства
 let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
 itemsR.forEach(item => {
   gsap.fromTo(item, { opacity: 0, x: 50 }, {
@@ -42,8 +46,9 @@ itemsR.forEach(item => {
       trigger: item,
       start: '-750',
       end: 'top',
-      scrub: true
+      scrub: true,
+      // Ограничение анимации на мобильных устройствах
+      onEnter: () => { if (ScrollTrigger.isTouch === 1) gsap.to(item, { opacity: 1, x: 0 }); }
     }
   });
 });
-	
